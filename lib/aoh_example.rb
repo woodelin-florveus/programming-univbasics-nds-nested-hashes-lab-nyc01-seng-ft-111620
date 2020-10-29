69398bf827bca46e639c35efa70afc9bc43b2192
Woodelin Florveus <woodelinflorveus@gmail.com>
2020-10-29 20:05:35 +0000

    Automatically backed up by Learn

--- lib/aoh_example.rb
+++ lib/aoh_example.rb
@@ -22,16 +22,13 @@ end
 def literal_aoh
   # Using Array literal syntax only, build a nested array that uses the data in
   # held in the constants
- aoh = [
-    [
-      DON_G,
-      JOELLE_VD,
-      PAT_M,
-      KATE_G,
-      BRUCE_G
-    ]
-  ]
-aoh.sort
+[
+  DON_G = { name:  "Don Gately", occupation: "Live-in Staff" }
+  JOELLE_VD = { name:  "Joelle van Dyne", occupation: "Radio Personality" }
+  PAT_M =  { name:  "Pat Monteseian", occupation: "Staff" }
+  KATE_G = { name:  "Kate Gompert", occupation: "None" }
+  BRUCE_G = { name:  "Bruce Green", occupation: "Fan of Mildred" }
+]
 
 end
 
