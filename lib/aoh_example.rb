3027d27691ecdaab9cf0985efcd95900d2d05724
Woodelin Florveus <woodelinflorveus@gmail.com>
2020-10-29 20:00:59 +0000

    Automatically backed up by Learn

--- lib/aoh_example.rb
+++ lib/aoh_example.rb
@@ -21,18 +21,16 @@ end
 def literal_aoh
   # Using Array literal syntax only, build a nested array that uses the data in
   # held in the constants
-  # [
-  #   [
-  #     DON_G,
-  #     JOELLE_VD,
-  #     PAT_M,
-  #     KATE_G,
-  #     BRUCE_G
-  #   ]
-  # ]
+  new = [
+    [
+      DON_G,
+      JOELLE_VD,
+      PAT_M,
+      KATE_G,
+      BRUCE_G
+    ]
+  ]
 
-  new_arr = [all]
-  new_arr
 end
 
 def aoh_lookup(aoh, row, key)
